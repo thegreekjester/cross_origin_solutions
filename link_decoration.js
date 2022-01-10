d7d8a812ba0a2f5d9a832499edb18db4f76f6fd8
Peter Katsos <peterlouiskatsos@gmail.com>
2022-01-10 13:25:12 -0600

    Update link_decoration.js

--- link_decoration.js
+++ link_decoration.js
@@ -1,80 +1,60 @@
-
-window["optimizely"] = window["optimizely"] || [];
-window["optimizely"].push({
-    type: "addListener",
-    filter: {
-        type: "lifecycle",
-        name: "activated"
-    },
-    handler: onActivated
-});
-
-// Check if enduserID is in URL 
-if (window.location.href.indexOf('opt_id') !== -1) {
-
-
-    var queryId = getUrlParameter('opt_id');
-
-    if (queryId !== endUserId) {
-        var topLevel = window.location.hostname.split('.').slice(-2).join('.');
-        setCookie('optimizelyEndUserId', queryId, 60, topLevel);
-
-    }
-
-}
-
-function onActivated() {
-
-    // Provide top level / sub domain changes 
-    var domains = ['playhearthstone.com'];
-
-    // EndUserID
-    var endUserId = getCookie('optimizelyEndUserId');
-
-    // Create empty <a> list
-    var a_tags = [];
-
-    document.addEventListener("DOMContentLoaded", function (event) {
-        // Push all <a> with an href that includes one of the provided domains
-        for (var i = 0; i < domains.length; i++) {
-            var matches = document.querySelectorAll("a[href*='" + domains[i] + "'");
-
-            a_tags = a_tags.concat(matches);
-        }
-
-        a_tags[0].forEach(function (tag) {
-            tag.href += (tag.href.split('?')[1] ? '&' : '?') + 'opt_id=' + endUserId;
-        });
-    });
-
-
-}
-
-// Functions required for this to work 
-function getCookie(name) {
+window.getCookie = function(name) {
     var match = document.cookie.match(name + '=([^;]*)');
     return match ? match[1] : undefined;
-};
-
-
-function setCookie(name, value, days, domain) {
+  };
+  
+  
+window.setCookie = function(name, value, days, domain) {
     var d = new Date();
     d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days);
     document.cookie = name + "=" + value + ";domain=" + domain + ";path=/;expires=" + d.toGMTString();
-}
-
-
-function getUrlParameter(sParam) {
+  };
+  
+  
+window.getUrlParameter = function(sParam) {
     var sPageURL = window.location.search.substring(1),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
-
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
+  
     for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-        }
+      sParameterName = sURLVariables[i].split('=');
+  
+      if (sParameterName[0] === sParam) {
+        return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+      }
     }
-};
+  };
+
+
+// Provide top level / sub domain changes
+var endUserId = window.getCookie('optimizelyEndUserId');
+
+// Check if enduserID is in URL
+if (window.location.href.indexOf('opt_id') !== -1) {
+
+    var queryId = getUrlParameter('opt_id');
+  
+    if (queryId !== endUserId) {
+      var topLevel = window.location.hostname.split('.').slice(-2).join('.');
+      window.setCookie('optimizelyEndUserId', queryId, 60, topLevel);
+  
+  }
+}
+ 
+window.addEventListener("load", function(){
+  console.log('content loaded');
+    var endUserId = window.optimizely ? window.optimizely.get('visitor').visitorId : window.getCookie('optimizelyEndUserId');
+    var domains = ['account.bellmedia.ca'];
+    var a_tags = [];
+    for (var i = 0; i < domains.length; i++) {
+      var matches = document.querySelectorAll("a[href*='" + domains[i] + "'");
+      a_tags = a_tags.concat(matches);
+    }
+
+    a_tags[0].forEach(function(tag) {
+      tag.href += (tag.href.split('?')[1] ? '&' : '?') + 'opt_id=' + endUserId;
+    });
+});
+
+
